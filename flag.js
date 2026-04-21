d66e690997a1a999bbd830e8dca2c92381ec3b6f
akowalczyk1235 <akowalczyk1235@gmail.com>
2026-04-21 07:42:51 +0200

    Update flag.js

--- flag.js
+++ flag.js
@@ -22,9 +22,9 @@ document.getElementById('okButton').addEventListener('click', function () {
     var komunikat13hElement = document.getElementById('komunikat13h');
 
     function formatujDate(date) {
-        var dzien = ("0" + date.getDate()).slice(-2);
-        var miesiac = ("0" + (date.getMonth() + 1)).slice(-2);
-        return dzien + "." + miesiac;
+        var d = ("0" + date.getDate()).slice(-2);
+        var m = ("0" + (date.getMonth() + 1)).slice(-2);
+        return d + "." + m;
     }
 
     function formatujDoISO(date) {
@@ -33,10 +33,8 @@ document.getElementById('okButton').addEventListener('click', function () {
 
     async function wyslijDoKalendarza(tytul, start, koniec) {
         const webhookURL = "https://hook.eu1.make.com/mdjizitdhmvgiioqfsl64mj2hr5fqpjj"; 
-                        
         
-        // Google Calendar traktuje datę końcową jako "do godziny 00:00 tego dnia"
-        // Więc żeby post 1-5 maja był widoczny cały 5 maja, musimy wysłać datę 6 maja
+        // Korekta dla Google Calendar (data końcowa +1 dzień)
         let dateEnd = new Date(koniec);
         dateEnd.setDate(dateEnd.getDate() + 1);
 
@@ -50,20 +48,26 @@ document.getElementById('okButton').addEventListener('click', function () {
                     end: formatujDoISO(dateEnd)
                 })
             });
-        } catch (e) { console.error("Błąd wysyłki", e); }
+            console.log("Wysłano do Make: " + tytul);
+        } catch (e) { 
+            console.error("Błąd wysyłki dla " + tytul, e); 
+        }
     }
 
     if (!isNaN(dataStartu.getTime())) {
-        var dataKoncowa = new Date(dataStartu);
-        dataKoncowa.setDate(dataKoncowa.getDate() + 5);
-        komunikatElement.textContent = "W dniach " + formatujDate(dataStartu) + " - " + formatujDate(dataKoncowa) + " pościsz 15h";
-        wyslijDoKalendarza("15h", dataStartu, dataKoncowa);
+        // 1. Etap 15h
+        var dataKoncowa15h = new Date(dataStartu);
+        dataKoncowa15h.setDate(dataKoncowa15h.getDate() + 5);
+        komunikatElement.textContent = "W dniach " + formatujDate(dataStartu) + " - " + formatujDate(dataKoncowa15h) + " pościsz 15h";
+        wyslijDoKalendarza("15h", dataStartu, dataKoncowa15h);
 
+        // 2. Etap 24h
         var data24h = new Date(dataStartu);
         data24h.setDate(data24h.getDate() + 6);
         komunikat24hElement.textContent = "W dniu " + formatujDate(data24h) + " pościsz 24h";
         wyslijDoKalendarza("24h", data24h, data24h);
         
+        // 3. Etap 17h
         var dataPoczatek17h = new Date(dataStartu);
         dataPoczatek17h.setDate(dataPoczatek17h.getDate() + 7);
         var dataKoniec17h = new Date(dataStartu);
@@ -71,38 +75,35 @@ document.getElementById('okButton').addEventListener('click', function () {
         komunikat17hElement.textContent = "W dniach " + formatujDate(dataPoczatek17h) + " do " + formatujDate(dataKoniec17h) + " pościsz 17h";
         wyslijDoKalendarza("17h", dataPoczatek17h, dataKoniec17h);
 
-        var dataPoczatek15h = new Date(dataStartu);
-        dataPoczatek15h.setDate(dataPoczatek15h.getDate() + 10);
-        var dataKoniec15h = new Date(dataStartu);
-        dataKoniec15h.setDate(dataKoniec15h.getDate() + 15);
-        komunikat15hElement.textContent = "W dniach " + formatujDate(dataPoczatek15h) + " do " + formatujDate(dataKoniec15h) + " pościsz 15h";
-         wyslijDoKalendarza("15h", dataPoczatek15h, dataKoniec15h);
+        // 4. Drugi etap 15h
+        var dataPoczatek15h_2 = new Date(dataStartu);
+        dataPoczatek15h_2.setDate(dataPoczatek15h_2.getDate() + 10);
+        var dataKoniec15h_2 = new Date(dataStartu);
+        dataKoniec15h_2.setDate(dataKoniec15h_2.getDate() + 15);
+        komunikat15hElement.textContent = "W dniach " + formatujDate(dataPoczatek15h_2) + " do " + formatujDate(dataKoniec15h_2) + " pościsz 15h";
+        wyslijDoKalendarza("15h", dataPoczatek15h_2, dataKoniec15h_2);
 
+        // 5. Drugi etap 24h
         var data24h_2 = new Date(dataStartu);
         data24h_2.setDate(data24h_2.getDate() + 16);
         komunikat24hElement2.textContent = "W dniu " + formatujDate(data24h_2) + " pościsz 24h";
         wyslijDoKalendarza("24h", data24h_2, data24h_2);
-        
 
+        // 6. Drugi etap 17h (Tutaj był błąd w nazwie zmiennej - JUŻ POPRAWIONE)
         var dataPoczatek17h_2 = new Date(dataStartu);
         dataPoczatek17h_2.setDate(dataPoczatek17h_2.getDate() + 17);
         var dataKoniec17h_2 = new Date(dataStartu);
         dataKoniec17h_2.setDate(dataKoniec17h_2.getDate() + 19);
         komunikat17hElement2.textContent = "W dniach " + formatujDate(dataPoczatek17h_2) + " do " + formatujDate(dataKoniec17h_2) + " pościsz 17h";
-        wyslijDoKalendarza("17h", data17h_2, data17h_2);
+        wyslijDoKalendarza("17h", dataPoczatek17h_2, dataKoniec17h_2);
 
+        // 7. Etap 13h
         var dataPoczatek13h = new Date(dataStartu);
         dataPoczatek13h.setDate(dataPoczatek13h.getDate() + 20);
-
-        // Opcjonalnie: ustawiamy koniec na 7 dni później, żeby widzieć to w kalendarzu jako pasek
         var dataKoniec13h = new Date(dataPoczatek13h);
         dataKoniec13h.setDate(dataKoniec13h.getDate() + 7); 
-
         komunikat13hElement.textContent = "Od dnia " + formatujDate(dataPoczatek13h) + " pościsz max 13h";
-
-        // Wysyłamy przedział czasu (od 20. do 27. dnia)
         wyslijDoKalendarza("13h", dataPoczatek13h, dataKoniec13h);
-        
 
     } else {
         komunikatElement.textContent = "Podana wartość nie jest prawidłową datą.";
